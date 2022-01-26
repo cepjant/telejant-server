50e8761e74708c959238554be0ccd5420a024a82
cepjant <serg.vernigora@gmail.com>
2022-01-26 14:52:30 +1000

    добавил совместимость со старым .env параметром SERVICE_URL

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
-TARGET_SYSTEM_URL = os.getenv('TARGET_SYSTEM_URL')
 MAX_UPLOAD_FILE_SIZE_KB = os.getenv('MAX_UPLOAD_FILE_SIZE_KB', None)
+TARGET_SYSTEM_URL = os.getenv('TARGET_SYSTEM_URL') or os.getenv('SERVICE_URL')
