4d820a462245b4bbc3552351c4b91e6ae4c8f135
cepjant <vernigor191919@gmail.com>
2023-03-16 14:04:41 +1000

    рефактор

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 """ Запуск сервера и клиента телеграм """
 
-import asyncio
 import aiohttp
 from aiohttp import web
 from pathlib import Path
@@ -8,24 +7,25 @@ from pathlib import Path
 from middlewares import allowed_hosts_middleware
 from routes import routes
 
-app = web.Application(middlewares=[allowed_hosts_middleware, ])
-
 
 async def on_shutdown(application):
     """ Обработчик останова сервера """
     await application['session'].close()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
 
-    app.add_routes(routes)
-    app.on_shutdown.append(on_shutdown)
-
-    # сессия для связи с посредником
-    session = aiohttp.ClientSession()
-    app['session'] = app.get('session', session)
-
+async def on_startup(application):
     # проверяем что директория для хранения телеграм сессий создана или создаем ее
     Path("telegram/sessions").mkdir(exist_ok=True)
 
-    web.run_app(app, loop=loop)
+    # сессия для связи с посредником
+    session = aiohttp.ClientSession()
+    application['session'] = app.get('session', session)
+
+    application.add_routes(routes)
+    application.on_shutdown.append(on_shutdown)
+
+
+if __name__ == '__main__':
+    app = web.Application(middlewares=[allowed_hosts_middleware, ])
+    app.on_startup.append(on_startup)
+    web.run_app(app)
